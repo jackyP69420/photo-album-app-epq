c9e425aa9436c1aafc725fb56cfeecbc762fa4e1
jackyP69420 <jmp.09@icloud.com>
2026-09-01 11:52:07 +0100

    Initial commit: Create Xcode Swift project with SwiftData

---
+++ WeddingAlbum/WeddingAlbum/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  WeddingAlbum
+//
+//  Created by Jack Pattison on 01/09/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
